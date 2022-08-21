3db42ae1e9c5fd598ad9e1ca7dfd53eaecfe6f4c
jhhyung <jinhahyung@gmail.com>
2022-08-21 17:26:12 +0900

    회원가입창에 들어갈 Agreement 컴포넌트 뼈대

---
+++ src/components/Join/Agreement.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+// import styled from "styled-components";
+
+function Agreement() {
+  return (
+    <div style={{ textAlign: "center", margin: "20px 0" }}>
+      시간이 허락한다면....하겠습니다
+    </div>
+  );
+}
+
+export default Agreement;
