d12dad9c8204a9002b181fb54b9812d67adbef28
jhhyung <jinhahyung@gmail.com>
2022-08-24 19:59:47 +0900

    로그인slice에 instance적용

--- src/redux/modules/loginSlice.jsx
+++ src/redux/modules/loginSlice.jsx
@@ -1,6 +1,5 @@
 import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 import { instance } from "./instance";
-import axios from "axios";
 
 const initialState = {};
 
@@ -8,19 +7,12 @@ const initialState = {};
 export const loginThunk = createAsyncThunk(
   "loginSlice/loginThunk",
   async (payload, thunkAPI) => {
-    console.log(payload);
     try {
-
-      const response = await axios.post(
-        "http://52.78.13.173/user/login",
-        payload
-      );
-      localStorage.setItem("token", response.data.token);
-      alert("로그인성공");
-
+      const response = await instance.post("/user/login", payload);
+      localStorage.setItem("token", response.data.token); // 로그인 요청을 보낸 후 response에 담긴 token을 로컬스토리지에 저장
       return thunkAPI.fulfillWithValue(response.data);
     } catch (error) {
-      console.error(error);
+      alert(error.response.data.errorMessage); // 에러 발생시 메시지 출력
       return thunkAPI.rejectWithValue(error.response.data);
     }
   }
